8dcdfc090139a03d740782969c63259603580a6a
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-12 16:29:00 +0330

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,13 @@
 """
 models package
 
-This package contains all application models.
+Export all public application models.
 """
 
+from .base import BaseSchema
 from .product import Product
 
 __all__ = [
+    "BaseSchema",
     "Product",
 ]
