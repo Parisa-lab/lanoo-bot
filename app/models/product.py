3df936938a5037c7370e422def408917e3d04cb8
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-15 00:21:26 +0330

    Update product.py

--- app/models/product.py
+++ app/models/product.py
@@ -15,37 +15,30 @@ from pydantic import Field
 class Product(BaseModel):
     """
     Product model.
-
-    Represents a product found by a scraper.
     """
 
-    # Product title.
     title: str = Field(
         ...,
         description="Product title",
     )
 
-    # Store name.
     store: str = Field(
         ...,
         description="Store name",
     )
 
-    # Product price.
     price: int = Field(
         ...,
         ge=0,
         description="Price in toman",
     )
 
-    # Product URL.
     url: str = Field(
         default="",
         description="Product URL",
     )
 
-    # Product image URL.
     image_url: str = Field(
         default="",
-        description="Product image",
+        description="Product image URL",
     )
