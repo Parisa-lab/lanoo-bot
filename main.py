bbfddf4e7ee4bcd8986496cfb728ccdeaf35710c
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-15 15:30:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-"""
-main.py
-
-Application entry point.
-"""
-
 from app.bot import LanooBot
 
-
-def main() -> None:
-    """
-    Start Telegram bot.
-    """
+if __name__ == "__main__":
 
     bot = LanooBot()
+
     bot.run()
-
-
-if __name__ == "__main__":
-    main()
