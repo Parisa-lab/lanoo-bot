25cb17767276bea5507ed7158808e103c4ff32a7
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-15 10:49:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """
+"""
 main.py
 
 Application entry point.
@@ -70,3 +71,23 @@ if __name__ == "__main__":
     print(result)
 
     # main()
+"""
+
+
+"""
+main.py
+"""
+
+import asyncio
+from app.scrapers.torob import get_price
+
+
+if __name__ == "__main__":
+
+    result = asyncio.run(
+        get_price(
+            "https://torob.com/p/f498b27b-596c-47c8-a48d-0beed264b2d8/"
+        )
+    )
+
+    print(result)
