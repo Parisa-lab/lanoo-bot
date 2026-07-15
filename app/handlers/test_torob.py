6383dd3fda17ceaa3a262dc325694eaa32f083e3
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-15 10:54:54 +0330

    Create test_torob.py

---
+++ app/handlers/test_torob.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from app.scrapers.torob import get_price
+
+
+async def torob_test(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+) -> None:
+
+    url = (
+        "https://torob.com/p/"
+        "f498b27b-596c-47c8-a48d-0beed264b2d8/"
+    )
+
+    data = await get_price(url)
+
+    text = (
+        f"📦 {data['title']}\n\n"
+        f"💰 {data['price']}\n"
+        f"🏪 {data['seller']}\n"
+        f"🖼 {data['image']}"
+    )
+
+    await update.message.reply_text(text)
