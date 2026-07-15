1895c7afad2d5c9c8491c1996bcbb8cdc6dd6b1d
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-15 14:19:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 """
 main.py
+
+Application entry point.
 """
 
-import asyncio
-from app.scrapers.torob import get_price
+from app.bot import LanooBot
 
+
+def main() -> None:
+    """
+    Start Telegram bot.
+    """
+
+    bot = LanooBot()
+    bot.run()
 
 
 if __name__ == "__main__":
-
-    result = asyncio.run(
-        get_price(
-            "https://torob.com/p/f498b27b-596c-47c8-a48d-0beed264b2d8/"
-        )
-    )
-
-    print(result)
+    main()
