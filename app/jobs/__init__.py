729127a5fc89f3fd3913d73adb16521f6590cbbc
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-16 10:56:28 +0330

    Update __init__.py

--- app/jobs/__init__.py
+++ app/jobs/__init__.py
@@ -6,6 +6,9 @@ from app.jobs.price_monitor import monitor_price
 
 
 def register_jobs(application):
+    """
+    Register scheduled jobs.
+    """
 
     application.job_queue.run_repeating(
         monitor_price,
