67255d9e985b33f56820e5dde9dcf8ecbc753b5d
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-16 12:41:19 +0330

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,35 +1,21 @@
+"""
+start.py
+"""
+
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from app.scrapers.torob import get_price
 
-
-async def start(
-
-    logger.info("START COMMAND RECEIVED")
-
-
-
+async def start_command(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
 ) -> None:
 
+    if not update.message:
+        return
+
     await update.message.reply_text(
-        "Starting Torob Test"
+        "Welcome to Lanoo Bot.\n\n"
+        "Usage:\n"
+        "/price <torob_url>"
     )
-
-    url = (
-        "https://torob.com/p/"
-        "f498b27b-596c-47c8-a48d-0beed264b2d8/"
-    )
-
-    data = await get_price(url)
-
-    text = (
-        f"📦 {data['title']}\n\n"
-        f"💰 {data['price']}\n"
-        f"🏪 {data['seller']}\n\n"
-        f"🖼 {data['image']}"
-    )
-
-    await update.message.reply_text(text)
