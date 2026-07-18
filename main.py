777cb571fc946532b0586e2dc39c1a1f74ce11a1
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-18 10:24:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from app.config import settings
 from app.bot import LanooBot
 
 if __name__ == "__main__":
+
+    print("BOT_TOKEN =", settings.bot_token)
+    print("ENVIRONMENT =", settings.environment)
+    print("DEBUG =", settings.debug)
 
     bot = LanooBot()
 
