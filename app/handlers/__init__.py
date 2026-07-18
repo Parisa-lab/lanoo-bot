0765b6353083f0eb07fd0253fbbde1fce4acc351
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-18 09:55:36 +0330

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,17 +1,36 @@
 """
 handlers package
+
+Register all Telegram handlers here.
 """
+
+# ==========================================================
+# Third-Party Imports
+# ==========================================================
 
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 
+# ==========================================================
+# Local Imports
+# ==========================================================
+
 from app.handlers.start import start_command
+from app.handlers.help import help_command
 from app.handlers.price import price_command
 
+# ==========================================================
+# Registration
+# ==========================================================
 
 def register_handlers(
     application: Application,
 ) -> None:
+    """
+    Register all Telegram handlers.
+    """
+
+    print("REGISTER_HANDLERS CALLED")
 
     application.add_handler(
         CommandHandler(
@@ -22,7 +41,18 @@ def register_handlers(
 
     application.add_handler(
         CommandHandler(
+            "help",
+            help_command,
+        )
+    )
+
+    application.add_handler(
+        CommandHandler(
             "price",
             price_command,
         )
     )
+
+    print("START HANDLER REGISTERED")
+    print("HELP HANDLER REGISTERED")
+    print("PRICE HANDLER REGISTERED")
