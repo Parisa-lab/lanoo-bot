381aa0c1a43e948779417b2dcbd9a510adee3566
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-20 17:33:59 +0330

    Update add.py

--- app/handlers/add.py
+++ app/handlers/add.py
@@ -2,44 +2,41 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from app.scrapers.torob import get_price
-from app.storage.database import (
-    load_products,
-    save_products,
-)
-
+from app.database.repository import add_product
 
 async def add_command(
-    update: Update,
-    context: ContextTypes.DEFAULT_TYPE,
+update: Update,
+context: ContextTypes.DEFAULT_TYPE,
 ):
 
-    if not update.message:
-        return
+if not update.message:
+    return
 
-    if not context.args:
-
-        await update.message.reply_text(
-            "Usage:\n/add URL"
-        )
-        return
-
-    url = context.args[0]
-
-    data = await get_price(url)
-
-    products = load_products()
-
-    products.append(
-        {
-            "chat_id": update.message.chat_id,
-            "url": url,
-            "title": data["title"],
-            "price": data["price"],
-        }
-    )
-
-    save_products(products)
+if not context.args:
 
     await update.message.reply_text(
-        f"Added:\n{data['title']}"
+        "Usage:\n/add URL"
     )
+    return
+
+url = context.args[0]
+
+data = await get_price(url)
+
+if not data:
+
+    await update.message.reply_text(
+        "Unable to fetch product."
+    )
+    return
+
+await add_product(
+    chat_id=update.message.chat_id,
+    url=url,
+    title=data["title"],
+    price=data["price"],
+)
+
+await update.message.reply_text(
+    f"Added:\n{data['title']}"
+)
