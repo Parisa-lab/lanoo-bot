83a5ffa1d1c0229ce6ed36b37356e0cd59dc2fb6
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-20 17:34:29 +0330

    Update list_products.py

--- app/handlers/list_products.py
+++ app/handlers/list_products.py
@@ -1,44 +1,41 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from app.storage.database import load_products
-
+from app.database.repository import (
+get_products_by_chat,
+)
 
 async def list_command(
-    update: Update,
-    context: ContextTypes.DEFAULT_TYPE,
+update: Update,
+context: ContextTypes.DEFAULT_TYPE,
 ):
 
-    if not update.message:
-        return
+if not update.message:
+    return
 
-    products = load_products()
+products = await get_products_by_chat(
+    update.message.chat_id
+)
 
-    my_products = [
-        p
-        for p in products
-        if p["chat_id"]
-        == update.message.chat_id
-    ]
-
-    if not my_products:
-
-        await update.message.reply_text(
-            "No products found."
-        )
-        return
-
-    text = ""
-
-    for i, product in enumerate(
-        my_products,
-        start=1,
-    ):
-        text += (
-            f"{i}. "
-            f"{product['title']}\n"
-        )
+if not products:
 
     await update.message.reply_text(
-        text
+        "No products found."
     )
+    return
+
+text = ""
+
+for index, product in enumerate(
+    products,
+    start=1,
+):
+
+    text += (
+        f"{index}. "
+        f"{product.title}\n"
+    )
+
+await update.message.reply_text(
+    text
+)
