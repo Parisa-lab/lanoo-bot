65c9cf92e3793d707a67c534eb62e7bf630e19f0
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-27 17:11:16 +0330

    Update __init__.py

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -1,3 +1,13 @@
 """
-scrapers package
+app/scrapers/__init__.py
+
+Scrapers package exports.
 """
+
+from app.scrapers.torob import TorobScraper
+from app.scrapers.torob import get_price
+
+__all__ = [
+    "TorobScraper",
+    "get_price",
+]
