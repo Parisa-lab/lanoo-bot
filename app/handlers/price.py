237bfbb35c215dd5a7f038cc0c36ce29f3a86a7f
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-07-28 09:45:16 +0330

    Update price.py

--- app/handlers/price.py
+++ app/handlers/price.py
@@ -15,11 +15,11 @@ from telegram import InlineKeyboardMarkup
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from app.services.price_service import PriceService
+from app.container import price_service
 
 logger = logging.getLogger(__name__)
 
-price_service = PriceService()
+
 
 
 async def price_command(
